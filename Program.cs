1b806be05f12f00872487dcb994e2a546713cc07
unknown <3fredclark3@gmail.com>
2020-10-07 10:43:17 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using(var httpClient = new HttpClient())
+            {
+               var response = httpClient.GetAsync("https://pokeapi.co/api/v2/pokemon/ditto").Result;
+               var data = response.Content.ReadAsStringAsync().Result;
+               Console.WriteLine(data);
+            }
+        }
+    }
+}
